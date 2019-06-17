0404439573739c5e46fe8b109c9cbe7fd70a249c
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-06-17 15:32:04 -0400

    adds button that retreives all data from DB and creates marker components from db data

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import FetchLocation from './components/FetchLocation';
 import UsersMap from './components/UsersMap'
 export default class App extends React.Component {
+  state = {
+    userLocation: null,
+    usersPlaces: []
+  }
   getUserLocationHandler = () => {
+    // retrieves user location and saves in state
     navigator.geolocation.getCurrentPosition(position => {
       console.log(position)
+      this.setState({
+        userLocation: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0622,
+          longitudeDelta: 0.0421,
+        }
+      })
+      // Makes POST request to firebase database and sends user location to db
+      fetch('https://striking-decker-244017.firebaseio.com/places.json', {
+        method: 'POST',
+        body: JSON.stringify({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));  
     }, err => console.log(err));
+  }
 
+   // Makes GET request from firebase DB and retrieves all user location data
+  getUserPlacesHandler = () => {
+    fetch('https://striking-decker-244017.firebaseio.com/places.json')
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placesArray = [];
+        // for loop that populates placesArray to be passed down as props
+        for (const key in parsedRes) {
+          placesArray.push({
+            latitude: parsedRes[key].latitude,
+            longitude: parsedRes[key].longitude,
+            id:key
+          })
+        }
+        this.setState({
+          usersPlaces: placesArray
+        })
+      })
+      .catch(err => console.log(err));
   }
   render () {
-  return (
-    <View style={styles.container}>
-      <FetchLocation onGetLocation={this.getUserLocationHandler} />
-      <UsersMap />
-    </View>
-   );
+      return (
+        <View style={styles.container}>
+          <View style={{marginBottom:20}}>
+            <Button title="Get User Places" onPress={this.getUserPlacesHandler} />
+          </View>
+          <FetchLocation onGetLocation={this.getUserLocationHandler} />
+          <UsersMap userLocation={this.state.userLocation} usersPlaces={this.state.usersPlaces}/>
+        </View>
+      );
   }
 }
 
--- components/UsersMap.js
+++ components/UsersMap.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { View , StyleSheet} from 'react-native'; 
 import MapView from 'react-native-maps';
 const usersMap = props => {
+    let userLocationMarker = null; 
+    // Conditionally rends the userlocation marker. Set to null as default and populates on button press
+    if (props.userLocation) {
+        userLocationMarker = <MapView.Marker coordinate={props.userLocation} />;
+    }
+    // maps all userPlaces objects and creates marker out of all from db
+    const usersMarkers = props.usersPlaces.map(userPlace => (
+    <MapView.Marker coordinate={userPlace} key={userPlace.id} />
+    ));
     return (
         <View style={styles.mapContainer}>
-            <MapView style={styles.map}/>
+            <MapView 
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0622,
+                longitudeDelta: 0.0421,
+            }}
+            region={props.userLocation}
+            style={styles.map}>
+                {userLocationMarker}
+                {usersMarkers}
+            </MapView>
         </View>
     );
 };
@@ -12,7 +32,8 @@ const usersMap = props => {
 const styles = StyleSheet.create({
     mapContainer: {
         width: '100%',
-        height: 200
+        height: 200,
+        marginTop: 20
     },
     map: {
         width: '100%',
